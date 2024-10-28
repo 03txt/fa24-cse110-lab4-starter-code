befa5ce960ac553d0a2e149609a94d055737dedd
dania <146884362+03txt@users.noreply.github.com>
2024-10-28 08:59:37 -0700

    stARTing

--- src/components/Expense/AddExpenseForm.tsx
+++ src/components/Expense/AddExpenseForm.tsx
@@ -1,12 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { Expense } from "../../types/types";
+
 const AddExpenseForm = () => {
   // Exercise: Consume the AppContext here
 
   // Exercise: Create name and cost to state variables
 
+  const initialExpense = {
+    id: -1,
+    name: "",
+    cost: "",
+  };
+
+  const [expenseItem, setExpenseItem] = useState(initialExpense); 
+
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     // Exercise: Add add new expense to expenses context array
   };
 
@@ -20,9 +29,10 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="name"
-            value={""}
-            // HINT: onChange={}
-          ></input>
+            value={expenseItem.name}
+            onChange = {(event) =>
+              setExpenseItem({ ...expenseItem, name: event.target.value })}>
+          </input>
         </div>
         <div className="col-sm">
           <label htmlFor="cost">Cost</label>
@@ -31,9 +41,10 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="cost"
-            value={0}
-            // HINT: onChange={}
-          ></input>
+            value={expenseItem.cost}
+            onChange={(event) =>
+              setExpenseItem({ ...expenseItem, cost: event.target.value })}>
+          </input>
         </div>
         <div className="col-sm">
           <button type="submit" className="btn btn-primary mt-3">
